fdadf85df8bc78a014e7ce7b502bd163aaf23ce9
eMMing <minh0987ae@gmail.com>
2025-05-06 16:37:49 +0700

    Delete DIVIDE & CONQUER/DSA4006.cpp

--- DIVIDE & CONQUER/DSA4006.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-#define run() int t; cin>>t; while(t--)
-#define ll long long
-int main(){
-    
-}
